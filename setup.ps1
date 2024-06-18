a856a2de9bb32ffeb8bd668161640509837977d4
Avni Gashi <avnig92@gmail.com>
2024-06-18 22:50:39 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -282,20 +282,12 @@ function Install-Yarn-Pnpm {
     }
 }
 
-# Install Docker
-function Install-Docker {
+function Open-DockerDownloadPage {
     $url = "https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe"
-    $installerPath = "$env:TEMP\DockerInstaller.exe"
-    Invoke-WebRequest-Retry -url $url -outputPath $installerPath
-    Show-LoadingAnimation -message "Downloading Docker" -durationSeconds 10
-    Start-Process -FilePath $installerPath 
-    if ($?) {
-        Write-Host "Docker has been installed successfully."
-        Add-ToPath -newPath "C:\Program Files\Docker\Docker"
-    } else {
-        Write-Host "Failed to install Docker."
-    }
+    Start-Process $url
+    Write-Host "Docker download page has been opened in your browser."
 }
+
 
 # DMA klonen und einrichten
 function DMA-Klonen-und-Einrichten {
